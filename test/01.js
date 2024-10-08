469d2ef273081d7c6ca5038cf0669c39833b50c3
atif128873806 <155367228+atif128873806@users.noreply.github.com>
2024-10-08 10:38:10 +0000

    conversion of datatypes

---
+++ test/01.js
@@ -0,0 +1,9 @@
+const id= 7786811
+let username = "Atif"
+var password = "am383455"
+city = "qurtta"
+console.table([id, username, password, city])
+/* 
+here chai with code say that dose not use in modern javascript so let is more easy and give less errors
+A constant is a data item whose value cannot change during the program's execution 
+*/
---
+++ test/conversion.js
@@ -0,0 +1,26 @@
+// let score = "33abc"
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// "33" = 33
+/*
+"33abc" = NaN (not a number)
+true = 1, false = 0
+these are mainn points
+*/
+// let name = "atif"
+// let booleanIsLoggedIn= Boolean(name)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 = true, 0=false
+// "" = false
+// "atif"= true         main ponts
+
+let point = 33
+let stringNumber = String(point)
+console.log(typeof stringNumber)
+console.log(stringNumber)
