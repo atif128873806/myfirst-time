d283af64656691b82b09d963aaa488b7db3e85cd
atif128873806 <155367228+atif128873806@users.noreply.github.com>
2024-10-09 15:09:45 +0000

    strings

---
+++ test/05-strings.js
@@ -0,0 +1,9 @@
+const name = "atif"
+const count = 50
+console.log(name + count)
+
+    console.log(`hello my name is ${name} and my repo count is ${count}`);
+
+    const gameName = new String('atif')
+    console.log(gameName)
+    console.log(typeof gameName)
