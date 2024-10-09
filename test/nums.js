8b98ac97027a1b3bde48bd78d706446d24dd7c74
atif128873806 <155367228+atif128873806@users.noreply.github.com>
2024-10-09 17:39:08 +0000

    math and number

---
+++ test/nums.js
@@ -0,0 +1,29 @@
+const score = 400
+// console.log(score)
+
+const points = new Number(670)
+// console.log(points)
+
+// console.log (points.toString().length)
+// console.log (points.toFixed(2))
+
+const othernumber = 123.899
+// console.log(othernumber.toPrecision(4))
+
+const value = 10000000
+// console.log(value.toLocaleString())
+// +++++++++++++++++++++maths++++++++++++++++++++++++++++++
+// console.log(Math)
+// console.log(Math.abs(-7))
+// console.log(Math.round(5.8))
+// console.log(Math.ceil(5.8))
+// console.log(Math.floor(5.8))
+
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) +1);
+
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() *(max - min +1)) +min)
