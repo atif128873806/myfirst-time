b0d39035baa9ce6c005dc4661da00b942df9dd22
atif128873806 <155367228+atif128873806@users.noreply.github.com>
2024-10-10 11:26:18 +0000

    date

---
+++ test/Date.js
@@ -0,0 +1,12 @@
+// let myDate = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toString());
+let specificDate = new Date(2024, 9, 10, 14, 30, 0);
+// console.log(specificDate.toDateString());
+let date = new Date()
+console.log(date.toLocaleString('default'{
+    month: "2-digit"
+}));
+
